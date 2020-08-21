fd41787004a8abd1ae198c466e5994d963f3d72e
Chen Stan <stan.chen@ssbshoes.com>
2020-08-21 16:15:04 +0700

    End of Section 6 (Use 3rd part libaries)

--- DatingApp-SPA/src/app/_services/error.interceptor.ts
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -32,7 +32,7 @@ export class ErrorInterceptor implements HttpInterceptor {
           if (serverError && typeof serverError === 'object') {
             for (const key in serverError) {
               if (serverError[key]) {
-                modalStateErrors += serverError[key] + '\n';
+                modalStateErrors += JSON.stringify(serverError[key]) + '\n';
               }
             }
           }
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -9,24 +11,18 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
-
+import { AlertifyService } from './_services/alertify.service';
 
 @NgModule({
-  declarations: [				
-    AppComponent,
-      NavComponent,
-      HomeComponent,
-      RegisterComponent
-   ],
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
   ],
-  providers: [
-    AuthService,
-    ErrorInterceptorProvider
-  ],
-  bootstrap: [AppComponent]
+  providers: [AuthService, ErrorInterceptorProvider, AlertifyService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
