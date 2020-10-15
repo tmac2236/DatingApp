4520865eb73c47256d66f0ebf1a2d270a7f23b0f
Chen Stan <stan.chen@ssbshoes.com>
2020-10-15 09:49:33 +0700

    fixed

--- DFPS-API/DTOs/NoOperationDto.cs
+++ DFPS-API/DTOs/NoOperationDto.cs
@@ -1,10 +1,14 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DFPS.API.DTOs
 {
     public class NoOperationDto
     {
         public DateTime? StartDate { get; set; }
         public string? Lean { get; set; }
+
+        [Column(TypeName = "decimal(18,4)")]
         public decimal? Status { get; set; }
         public int? IOrder { get; set; }
         public int? OnDuty { get; set; }
