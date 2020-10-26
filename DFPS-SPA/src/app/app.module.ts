d531f39b5a585ee43a91fe14c54cd932ff833644
Chen Stan <stan.chen@ssbshoes.com>
2020-10-26 12:27:56 +0700

    test

--- DFPS-SPA/src/app/app.module.ts
+++ DFPS-SPA/src/app/app.module.ts
@@ -75,7 +75,7 @@ export function tokenGetter() {
         allowedDomains: ['http://localhost:5000'],
         disallowedRoutes: ['http://localhost:5000/api/auth'],
         //allowedDomains: ['http://10.4.0.74:6969'],
-        //disallowedRoutes: ['http://10.4.0.74:6969/api/auth'],
+        //disallowedRoutes: ['http://10.4.0.74:6969/api/auth'], 
       },
     }),
   ],
